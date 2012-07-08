4ec12220fe703655427db68ff43a76eaefed1f9b
Stanislas Polu <stan@teleportd.com>
2012-07-08 23:01:02 +0200

    scales and style + full ms type graph

--- public/assets/js/c_graph_c.js
+++ public/assets/js/c_graph_c.js
@@ -42,9 +42,10 @@ var c_graph_c = function(spec, my) {
         else past.push(0);
       });
 
+      var mrg = 16;
       var y = d3.scale.linear().domain([d3.min([0].concat(today, past)), 
-                                        d3.max([].concat(today, past))]).range([0 + 10, 150 - 10]);
-      var x = d3.scale.linear().domain([0, past.length]).range([0 + 10, 460 - 10]);
+                                        d3.max([].concat(today, past))]).range([0 + mrg, 150 - mrg]);
+      var x = d3.scale.linear().domain([0, past.length]).range([0 + 2*mrg, 460 - mrg]);
 
       var vis = d3.select('#dattss-graph-' + my.idx)
         .append('svg:svg')
@@ -59,13 +60,41 @@ var c_graph_c = function(spec, my) {
         .y(function(d) { return -1 * y(d); });
       var area = d3.svg.area()
         .x(function(d, i) { return x(i); })
-        .y0(-9)
+        .y0(1-mrg)
         .y1(function(d, i) { return -1 * y(d); });
 
       g.append('svg:path').attr('d', line(past)).classed('past line', true);
       g.append('svg:path').attr('d', area(past)).classed('past area', true);
       g.append('svg:path').attr('d', line(today)).classed('today line', true);
       g.append('svg:path').attr('d', area(today)).classed('today area', true);
+
+      // DATE LABEL
+      g.selectAll('.xlabel')
+        .data(that.ticks(x, 5))
+        .enter().append('svg:text')
+        .attr('class', 'xlabel')
+        .text(function(v) {
+          var h = Math.floor(24 / x.domain()[1] * v);
+          return h + 'h';
+        })
+        .attr('x', function(d) { return x(d) })
+        .attr('y', 0)
+        .attr('text-anchor', 'middle');
+
+      // VALUE LABEL
+      g.selectAll('.ylabel')
+        .data(y.ticks(3))
+        .enter().append('svg:text')
+        .attr('class', 'ylabel')
+        .text(function(v) {
+          if(v > 1000000000) { return Math.floor(v / 1000000000) + 'b' }
+          if(v > 1000000) { return Math.floor(v / 1000000) + 'm' }
+          if(v > 1000) { return Math.floor(v / 1000) + 'k' }
+          return v;
+        })
+        .attr('x', 4)
+        .attr('y', function(d) { return -1 * y(d); })
+        .attr('dy', 2);
     }
 
     _super.refresh(json);
