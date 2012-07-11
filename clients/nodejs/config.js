52df25be0a973bc4eeeb7b3dcf18159441655c61
Stanislas Polu <stan@teleportd.com>
2012-07-11 15:05:49 +0200

    dattss host server update

--- clients/nodejs/config.js
+++ clients/nodejs/config.js
@@ -5,7 +5,7 @@ config['DATTSS_PUSH_PERIOD'] = 5 * 1000;
 config['DATTSS_PERCENTILE'] = 0.1;
 
 config['DATTSS_CLIENT_AUTH'] = 'not-configured';
-config['DATTSS_SERVER_HOST'] = 'dattss.teleportd.com';
+config['DATTSS_SERVER_HOST'] = 'agg.dattss.com';
 config['DATTSS_SERVER_PORT'] = 80;
 
 exports.config = config;
